c6a589890bbbdff4eb105e1f014c27015241bb87
Pavel Zhelnov <pzhelnov@p1m.org>
2025-04-14 19:36:43 -0400

    Upd store sources in test trifid to follow prod oxigraph
    This allows me to actually dereference URIs
    from prod oxigraph while trifid is running locally -
    thats actually quite exciting!
    
    Although not sure why it won't dereference graph URIs.

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ docker run --rm -it \
     -v "/mnt/c/home/anonymous/ao-gbad-trifid/instances/gbad/config.yaml:/instances/gbad/config.yaml" \
     -v "/mnt/c/home/anonymous/ao-gbad-trifid/instances/gbad/welcome.hbs:/instances/gbad/welcome.hbs" \
     -v "/mnt/c/home/anonymous/ao-gbad-trifid/instances/gbad/render.hbs:/instances/gbad/render.hbs" \
-    -v "/mnt/c/home/anonymous/ao-gbad-trifid/data/www/statements.trig:/data/statements.trig" \
+    -v "/mnt/c/Users/Anonymous/Workshop/202312161720UTC-5_RA_at_GLAM_Incubator/Some_files_from_OneDrive_2025-03-17_onward/merged/kb.pz.2025-04-14_0307_UTC-4.nq:/upload/store.nq" \
     ghcr.io/zazuko/trifid
